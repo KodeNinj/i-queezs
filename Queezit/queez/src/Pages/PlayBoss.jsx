8c67f2ba2f63763f96463c6ca2432c33bcf437db
KodeNinja <79848529+KodeNinj@users.noreply.github.com>
2025-03-03 13:47:58 +0100

    Update PlayBoss.jsx
    updated the naming of db

--- Queezit/queez/src/Pages/PlayBoss.jsx
+++ Queezit/queez/src/Pages/PlayBoss.jsx
@@ -1,15 +1,15 @@
-import React from "react";
-import Navbar from "../Components/Navbar";
-import { CountriesCapitalBossObj } from "../Data/CountriesCapitalBossObj";
-import Game from "../Components/Game";
-import bg from "../Assets/design.png";
+import React from 'react';
+import Navbar from '../Components/Navbar';
+import { CountriesCapitalBossObj } from '../Data/CountriesCapitalBossObj';
+import Game from '../Components/Game';
+import bg from '../Assets/design.png';
 const PlayBoss = () => {
 	return (
-		<section className='bg-[var(--primaryBg)] h-[105vh] z-30 pb-[80px] overflow-auto'>
+		<section className='bg-[var(--primaryBg)] min-h-screen  z-30 pb-[40px] overflow-auto'>
 			<Navbar />
-			<Game gameLevel={CountriesCapitalBossObj} />
+			<Game gameData={CountriesCapitalBossObj} />
 			{/* The infrastructure bg */}
-			<div className='absolute z-0  bottom-0 top-0 md:top-0 md:w-[100vw] w-[200vw] right-0 h-[100vh] object-fill overflow-y-hidden'>
+			<div className='absolute z-0  bottom-0 top-[1vh] md:top-0 md:w-[100vw] w-[200vw] right-0 h-[100vh] object-fill overflow-y-hidden'>
 				<img
 					src={bg}
 					alt='bg'
